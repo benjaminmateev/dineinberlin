6b1f089ea4fc4a3bc76ab8e8482953e66ee30c1d
Benjamin Mateev <benjamin.mateev@gmail.com>
2020-03-30 12:13:39 +0200

    updating about and changing zoom level again

--- pages/about.js
+++ pages/about.js
@@ -20,20 +20,14 @@ export default () => {
             </h2>
             <p className="max-w-xl text-navy-light text-lg mb-4">
               {content.description}
-              <a href="https://dineineberlin.com" target="_blank" rel="noopener">
-                dineinberlin.com
+              <a href="https://dinecph.dk" target="_blank" rel="noopener">
+                dinecph.dk
               </a>
               .
             </p>
             <p className="max-w-xl text-navy-light text-lg mb-4">
               {content.contact}
-              <a href="mailto:martin@pelion.app">martin@pelion.app</a>.
-            </p>
-            <p className="max-w-xl text-navy-light text-lg">
-              {content.webmaster}
-              <a href="mailto:sebastianwinther@gmail.com">
-                sebastianwinther@gmail.com
-              </a>
+              <a href="mailto:benjamin.mateev@gmail.com">benjamin.mateev@gmail.com</a>.
               .
             </p>
           </div>
@@ -47,14 +41,12 @@ export default () => {
 const pageContent = {
   'de-DE': {
     title: 'Über uns',
-    description: `Die COVID-19 Krise hat Berlin's restaurant szene hart getroffen. Viele Establissements bieten Lieferungen ihrer normalen oder neuer Gerichte an. WIr haben diese Seite gebaut um ihnen zu helfen mehr Leute zu erreichen - Inspierrert von`,
-    contact: 'Contact',
-    webmaster: 'Technik ',
+    description: `Die COVID-19 Krise hat Berlin's restaurant szene hart getroffen. Viele Establissements bieten Lieferungen ihrer normalen oder neuer Gerichte an. Wir haben diese Seite gebaut um ihnen zu helfen mehr Leute zu erreichen - Inspierrert von`,
+    contact: 'Anfragen zur Seite oder Technik an ',
   },
   'en-GB': {
     title: 'About',
     description: `The COVID-19 crisis has hit the Berlin restaurant scene hard. Many establishments have started offering take-out. We've made this site to help spread the word — inspired by `,
-    contact: 'Most inquiries: ',
-    webmaster: 'Site feedback: ',
+    contact: 'Most inquiries and site feedback to ',
   },
 }
