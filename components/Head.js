287ef89de22eff9aece20c0bb53035e764935a0c
Benjamin Mateev <benjamin.mateev@gmail.com>
2020-04-01 11:31:37 +0200

    updating meta to work with LinkedIn

--- components/Head.js
+++ components/Head.js
@@ -4,6 +4,7 @@ export default ({ children }) => {
   const title = 'Dine In Berlin'
   const site_url = 'https://dineinberlin.com'
   const fb_app_id = "571317996836972"
+  const author = "Benjamin Mateev"
   const description =
     "Your local food joints are struggling during the current crisis — but they're still cooking! Help them keep the lights on, by getting take-out from the best restaurants in Berlin."
   return (
@@ -13,12 +14,13 @@ export default ({ children }) => {
       <title>{title}</title>
       <meta property="og:type" content="website" />
       <meta name="description" content={description} />
+      <meta name="author" content={author} />
       <meta property="fb:app_id" content={fb_app_id} />
       <meta property="og:title" content={title} />
       <meta property="og:site_name" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={site_url} />
-      <meta property="og:image" content="https://dineinberlin.com/og-image.png" />
+      <meta name="image" property="og:image" content="https://dineinberlin.com/og-image.png" />
       <meta name="twitter:card" content="summary_large_image" />
       {/* <meta name="twitter:site" content="" /> */}
       <meta name="twitter:title" content={title} />
