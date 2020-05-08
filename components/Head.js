d3f11bfb282d8a34cef563b8f6f59f00747251ff
Benjamin Mateev <benjamin.mateev@gmail.com>
2020-05-08 22:10:14 +0200

    fixing audit issue

--- components/Head.js
+++ components/Head.js
@@ -11,7 +11,7 @@ export default ({ children }) => {
     <Head>
       <meta charset='utf-8' />
       <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-      <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+      <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1' />
       <link rel='manifest' href='/manifest.json' />
       <link rel='apple-touch-icon' href='/apple-icon.png'></link>
       <meta name='theme-color' content='#ff8ba7' />
