c6434ded1ee9e25825d0359fc2edd78d11671679
Benjamin Mateev <benjamin.mateev@gmail.com>
2020-10-30 18:16:15 +0100

    adapting to post-covid categories

---
+++ pages/[category].js
@@ -0,0 +1,351 @@
+import { useContext, useState } from 'react'
+import { useRouter } from 'next/router'
+import Promise from 'promise-polyfill'
+import fetch from 'isomorphic-unfetch'
+
+import { LanguageContext } from '../..//components/LanguageSelector'
+import Head from '../../components/Head'
+import Nav from '../../components/Nav'
+import Footer from '../../components/Footer'
+import LoadingSpinner from '../../components/LoadingSpinner'
+import Link from 'next/link'
+
+const pageContent = {
+  'es-PE': {
+    title: 'Deliveries',
+    offersLabel: 'Productos',
+    neighbourhoodLabel: 'Distritos',
+    orderLabel: 'Ir a Página Web',
+  },
+}
+
+const ListItem = ({ restaurant, content }) => {
+  const id = restaurant.id || undefined
+  const name = restaurant.name || undefined
+  const address = restaurant.address || undefined
+  const description = restaurant.description || undefined
+  const offers = restaurant.offerings || undefined
+  const phone = restaurant.phone || undefined
+  const mobile = restaurant.mobile || undefined
+  const url = restaurant.url || undefined
+  const neighbourhoods = restaurant.neighbourhoods || undefined
+  const email = restaurant.email || undefined
+
+  return (
+    <li key={id} className="w-full md:w-1/2 p-3">
+      <div className="relative h-full flex flex-col items-start border border-sand overflow-hidden p-4 sm:p-8 lg:px-12">
+        <div className="flex-auto">
+          {name && <h3 className="text-xl sm:text-2xl mb-2">{name}</h3>}
+          {address && <p className="text-xs sm:text-sm mb-2">{address}</p>}
+          
+          
+          <p className="text-base font-semibold">
+            {phone && <a href={"tel:" + phone}>{phone}</a> }
+            {phone && mobile && <span> | </span>}
+            {mobile && <a href={"tel:" + mobile}>{mobile}</a> }
+          </p>
+          <p className="text-base font-medium mb-4">
+            {email && <a href={"mailto:" + email}>{email}</a> }
+          </p>
+          {description && (
+            <p className="max-w-xl text-sm sm:text-base mb-4">{description}</p>
+          )}
+          {offers && !!offers.length && (
+            <ul className="-m-1 mb-6">
+              {offers.sort().map(offer => (
+                <li
+                  key={offer}
+                  className="inline-block font-medium text-xs sm:text-sm bg-sand px-2 py-1 m-1"
+                >
+                  {offer}
+                </li>
+              ))}
+            </ul>
+          )}
+          {neighbourhoods && !!neighbourhoods.length && ( <p className="font-semibold text-2xs sm:text-sm mb-2">Distritos</p>)}
+          {neighbourhoods && !!neighbourhoods.length && (
+            <ul className="-m-1 mb-6">
+              {neighbourhoods.sort().map(neighbourhood => (
+                <li
+                  key={neighbourhood}
+                  className="inline-block font-medium text-xs sm:text-sm bg-sand px-2 py-1 m-1"
+                >
+                  {neighbourhood}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        {url &&
+          <a
+            href={url.includes('http') ? url : 'https://' + url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary text-sm sm:text-base"
+          >
+            {content.orderLabel}&nbsp;&nbsp;&nbsp;⟶
+          </a>
+        }
+      </div>
+    </li>
+  )
+}
+
+class List extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { restaurants: this.props.restaurants }
+  }
+  
+  componentDidMount() {
+    this.setState({restaurants: shuffle(this.state.restaurants)})
+  }
+
+  render () {
+    let restaurants =this.state.restaurants
+    return (
+      <ul className="flex flex-wrap -m-3">
+        {restaurants
+          // Filter for necessary content
+          .filter(
+            restaurant =>
+              restaurant.name &&
+              restaurant.description 
+          )
+          // Filter for delivery
+          .filter(restaurant =>
+            this.props.filterDelivery ? restaurant.delivery : true
+          )
+          // Filter for offers
+          .filter(restaurant =>
+            this.props.filterOffers && this.props.filterOffers.length
+              ? this.props.filterOffers.every(offer =>
+                  restaurant.offerings.includes(offer)
+                )
+              : true
+          )
+          // Filter for neighbourhoods
+          .filter(restaurant => {
+            return this.props.filterNeighbourhoods && this.props.filterNeighbourhoods.length && restaurant.neighbourhoods && restaurant.neighbourhoods.length
+            ? this.props.filterNeighbourhoods.every(neighbourhood =>
+                restaurant.neighbourhoods.includes(neighbourhood)
+              )
+            : true
+          })
+          .map(restaurant => (
+            <ListItem
+              key={restaurant.id}
+              restaurant={restaurant}
+              content={this.props.content}
+            />
+          ))}
+      </ul>
+    )
+  }
+}
+
+export default ({ restaurants, neighbourhoods, offers }) => {
+  const router = useRouter()
+  const { city } = router.query
+
+  const { language } = useContext(LanguageContext)
+  const content = pageContent[language]
+
+  const [filterOffers, setFilterOffers] = useState([])
+  const [filterNeighbourhoods, setFilterNeighbourhoods] = useState([])
+
+  if (restaurants && !!restaurants.length)
+    return (
+      <>
+        <Head />
+        <div className="min-h-screen flex flex-col">
+          <Nav />
+          <main className="flex-auto px-3 pt-8 sm:pt-16 pb-16">
+            <div className="max-w-6xl mx-auto">
+              <h2 className="flex-auto font-extrabold text-2xl sm:text-3xl leading-none mb-4 sm:mb-6">
+                {content.title} en {city} 
+              </h2>
+              <div className="flex flex-wrap sm:flex-no-wrap items-end -m-1 mb-6">
+                <div className="w-full flex flex-wrap items-center mb-4 sm:mb-0">
+                  <p className="w-full sm:w-auto font-bold m-1 mr-2">
+                    {content.offersLabel}
+                  </p>
+                  <div className="w-full flex flex-wrap items-center mb-4 sm:mb-0 md:max-w-6xl max-w-xl">
+                    {offers.sort().map(offer => {
+                      const isChecked = filterOffers.includes(offer)
+                      const handleChange = () => {
+                        if (isChecked) {
+                          const newOffers = [...filterOffers]
+                          newOffers.splice(newOffers.indexOf(offer), 1)
+                          setFilterOffers(newOffers)
+                        } else {
+                          setFilterOffers([...filterOffers, offer])
+                        }
+                      }
+                      return (
+                        <label
+                          key={offer}
+                          className={
+                            'inline-block font-medium border-2 border-navy cursor-pointer px-2 py-1 m-1' +
+                            (isChecked
+                              ? ' text-sand-light bg-navy'
+                              : ' text-navy')
+                          }
+                        >
+                          <input
+                            type="checkbox"
+                            checked={isChecked}
+                            onChange={handleChange}
+                            className="sr-only"
+                          />
+                          <span className="select-none">
+                            {offer}
+                          </span>
+                        </label>
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-wrap sm:flex-no-wrap items-end -m-1 mb-6">
+                <div className="w-full items-center mb-4 sm:mb-0">
+                  <p className="w-full sm:w-auto font-bold m-1 mr-2">
+                    {content.neighbourhoodLabel}
+                  </p>
+                  <div className="w-full flex flex-wrap items-center mb-4 sm:mb-0 md:max-w-6xl max-w-xl">
+                    {neighbourhoods.sort().map(neighbourhood => {
+                      const isChecked = filterNeighbourhoods.includes(neighbourhood)
+                      const handleChangeN = () => {
+                        if (isChecked) {
+                          const newNeighbourhoods = [...filterNeighbourhoods]
+                          newNeighbourhoods.splice(newNeighbourhoods.indexOf(neighbourhood), 1)
+                          setFilterNeighbourhoods(newNeighbourhoods)
+                        } else {
+                          setFilterNeighbourhoods([...filterNeighbourhoods, neighbourhood])
+                        }
+                      }
+                      return (
+                        <label
+                          key={neighbourhood}
+                          className={
+                            'inline-block font-small border-2 border-navy cursor-pointer px-2 py-1 m-1' +
+                            (isChecked
+                              ? ' text-sand-light bg-navy'
+                              : ' text-navy')
+                          }
+                        >
+                          <input
+                            type="checkbox"
+                            checked={isChecked}
+                            onChange={handleChangeN}
+                            className="sr-only"
+                          />
+                          <span className="select-none">{neighbourhood}</span>
+                        </label>
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
+              <List 
+                restaurants={restaurants}
+                filterOffers={filterOffers}
+                filterNeighbourhoods={filterNeighbourhoods}
+                content={content}
+              />
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </>
+    )
+  return (
+    <>
+      <Head />
+      <div className="min-h-screen flex flex-col">
+        <Nav />
+        <main className="flex-auto px-3 pt-8 sm:pt-16 pb-16">
+          <div className="max-w-6xl mx-auto">
+            <h2 className="flex-auto font-extrabold text-2xl sm:text-3xl leading-none mb-4 sm:mb-6">
+              {content.title} en {city} 
+            </h2>
+          </div>
+          <div className="w-full h-full flex items-center justify-center text-3xl text-pink">
+            <p>Estamos trabajando para agregar entregas en tu ciudad. <a href={"/submit/" + city}>Ayúdanos agregando</a></p>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: {city: 'lima'} },
+      { params: {city: 'arequipa'} },
+      { params: {city: 'trujillo'} },
+      { params: {city: 'chiclayo'} },
+      { params: {city: 'cusco'} },
+      { params: {city: 'piura'} },
+    ], fallback: false
+  }
+}
+
+export async function getStaticProps( paths ) {
+  const airtableApiKey = process.env.AIRTABLE_API_KEY
+  const airtableBaseKey = process.env.AIRTABLE_BASE_KEY
+
+  const Airtable = require('airtable')
+  const airtable = new Airtable({
+    apiKey: airtableApiKey,
+  }).base(airtableBaseKey)
+  const base = await airtable(paths.params.city)
+  const records = await base
+    .select({
+      maxRecords: 999999, // don't want to paginate...
+      view: 'Grid view', // NOTE: changing the view name will break things
+      fields: ['name', 'address', 'description', 'categories', 'phone','mobile', 'url', 'neighbourhoods', 'email'],
+      filterByFormula: "display = '1'",
+    })
+    .all()
+  const restaurants = await Promise.all(records.map(record => {
+    const info = record.fields
+    info.id = record.id
+    return info
+  }))
+
+  const neighbourhoods = Array.from(
+    new Set(
+      restaurants.reduce( (hoods, restaurant) => {
+        if(restaurant.neighbourhoods != undefined) hoods.push(restaurant.neighbourhoods)
+        return hoods
+      }, []).flat(Infinity)
+    )
+  )
+
+  const offers = Array.from(
+    new Set(
+      restaurants.reduce( (o, restaurant) => {
+        if(restaurant.offerings != undefined) o.push(restaurant.offerings)
+        return o
+      }, []).flat(Infinity)
+    )
+  )
+
+  return { props: { restaurants, neighbourhoods, offers } }
+}
+
+export function shuffle(arr) {
+  var i,
+        j,
+        temp;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;  
+}
